a04f32d00e38681f4092f5ecc4c0fe3381fde65f
aosante <aosantea@ucenfotec.ac.cr>
2021-04-06 23:01:28 -0600

    add icons

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,7 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const Create = () => {
   return (
@@ -20,6 +21,7 @@ const Create = () => {
         color="secondary"
         variant="contained"
         disableElevation
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
