8e6aec72a942e22939d032e1633c915c2273a53a
aosante <aosantea@ucenfotec.ac.cr>
2021-04-06 22:53:51 -0600

    add submit button component

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,29 @@
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 const Create = () => {
   return (
-    <div>
-      <Typography color="secondary">Create New Note</Typography>
-    </div>
+    <Container size="sm">
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('You clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        disableElevation
+      >
+        Submit
+      </Button>
+    </Container>
   );
 };
 
