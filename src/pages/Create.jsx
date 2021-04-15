2bb366763347ce35aa67af540f71ed659f42114f
aosante <aosantea@ucenfotec.ac.cr>
2021-04-14 23:30:37 -0600

    makeStyles example

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,12 +1,27 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  },
+});
+
 const Create = () => {
+  const classes = useStyles();
+
   return (
     <Container size="sm">
       <Typography
+        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -16,6 +31,7 @@ const Create = () => {
       </Typography>
 
       <Button
+        className={classes.btn}
         onClick={() => console.log('You clicked me')}
         type="submit"
         color="secondary"
