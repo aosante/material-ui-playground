b96ce08714ae80878ca192173410ca55ee82ded8
aosante <aosantea@ucenfotec.ac.cr>
2021-04-19 22:03:06 -0600

    add text field components

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+  },
+});
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const classes = useStyles();
+
   return (
     <Container size="sm">
       <Typography
@@ -14,6 +29,31 @@ const Create = () => {
       >
         Create a New Note
       </Typography>
+
+      <form noValidate autoComplete="off">
+        <TextField
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          className={classes.field}
+          fullWidth
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <TextField
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          className={classes.field}
+          multiline
+          rows={4}
+          fullWidth
+          required
+          onChange={(e) => setDetails(e.target.value)}
+          value={details}
+        />
+      </form>
 
       <Button
         onClick={() => console.log('You clicked me')}
