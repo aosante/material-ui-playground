77a47d840800af39d70a8ba4dbb09279bcb7a719
aosante <aosantea@ucenfotec.ac.cr>
2021-04-25 22:13:50 -0600

    install json-server and fetch data

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
+
 const Notes = () => {
-  return <div>Notes page</div>;
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/notes')
+      .then((res) => res.json())
+      .then((data) => {
+        setNotes(data);
+      });
+  }, []);
+  return (
+    <div>
+      {notes.map((note) => (
+        <p key={note.id}>{note.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Notes;
