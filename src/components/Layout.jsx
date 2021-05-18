687379add836cc06498e8b6ea3c07a3b5448d656
aosante <aosantea@ucenfotec.ac.cr>
2021-05-17 21:58:32 -0600

    add drawer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,39 @@
 import { makeStyles } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
   page: {
     width: '100%',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
 });
 const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar */}
-      {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variaint="h5">Ninja Notes</Typography>
+        </div>
+      </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
   );
